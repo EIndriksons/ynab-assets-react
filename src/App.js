4cbd20415d6bd8891fcbc80fee84dc0f0fc8352a
EIndriksons <edgars@indriksons.lv>
2020-10-14 21:28:52 +0300

    Extracting data from Ynab API

--- src/App.js
+++ src/App.js
@@ -2,22 +2,46 @@ import React, { Component } from 'react';
 import * as ynab from 'ynab';
 
 class App extends Component {
-  state = {}
+  state = {};
 
   componentDidMount() {
     const accessToken = process.env.REACT_APP_API_KEY;
     const ynabAPI = new ynab.API(accessToken);
 
-    ynabAPI.budgets.getBudgets().then(res => {
-      console.log(res);
-    })
+    ynabAPI.transactions
+      .getTransactionsByAccount(process.env.REACT_APP_BUDGET_ID, process.env.REACT_APP_ASSET_ID)
+      .then((res) => {
+        const transactions = [];
+        res.data.transactions.forEach((transaction) => {
+          let memo = transaction.memo;
+          let asset_id = null;
+          let asset_memo = null;
+
+          if (memo.includes('{')) {
+            memo = memo.match('^(.(?!{([^}]+)}))*')[0];
+            asset_id = transaction.memo.match('{([^}]+)}')[1].split(',')[0];
+            asset_memo = transaction.memo.match('{([^}]+)}')[1].split(',')[1];
+          }
+
+          transactions.push({
+            id: transaction.id,
+            date: transaction.date,
+            amount: transaction.amount / 100,
+            memo: memo,
+            asset_id: asset_id,
+            asset_event: asset_memo,
+          });
+        });
+
+        this.setState({ transactions: transactions });
+      });
   }
 
   render() {
     return (
-    <React.Fragment>
-      <div>Hello World! This is {process.env.REACT_APP_API_KEY}</div>
-    </React.Fragment>
+      <React.Fragment>
+        <div>Hello World! This is {process.env.REACT_APP_API_KEY}</div>
+      </React.Fragment>
     );
   }
 }
