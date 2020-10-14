720a2059de0c81f91ee7140e0bebdecd66eebc75
EIndriksons <edgars@indriksons.lv>
2020-10-14 20:21:05 +0300

    Implemented YNAB API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-
+import * as ynab from 'ynab';
 
 class App extends Component {
-    render() {
-        return (
-        <React.Fragment>
-          <div>Hello World!</div>
-        </React.Fragment>
-        );
-    }
+  state = {}
+
+  componentDidMount() {
+    const accessToken = process.env.REACT_APP_API_KEY;
+    const ynabAPI = new ynab.API(accessToken);
+
+    ynabAPI.budgets.getBudgets().then(res => {
+      console.log(res);
+    })
+  }
+
+  render() {
+    return (
+    <React.Fragment>
+      <div>Hello World! This is {process.env.REACT_APP_API_KEY}</div>
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;
